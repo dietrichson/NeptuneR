8d90eaea93f8415d6ba80d5f1ffeb93ecb06f9b2
vlandaberry <victorialandaberry@gmail.com>
2018-05-16 22:35:17 -0300

    Add files via upload

--- R/send_raw_query.R
+++ R/send_raw_query.R
@@ -1,10 +1,14 @@
-#' Send a Raw query to Neptune
+#' Send a Query to Neptune
 #'
 #' @param ep Endpoint
-#' @param query The Raw Query (in JSON) to be sent
-#'
+#' @param query The Query (in selected type) to be sent
+#' @param  type The type (language) of query to use
 #' @return the result from POST (in JSON)
 #' @export
-neptune_send_json_query <- function(ep,query){
-
+#' 
+neptune_send_json_query <- function(ep,query,type=c("gremlin","sparql"),as=c('parsed','text','raw'),...){
+  ifelse (type=="gremlin",jsonQuery<- paste0("{",'"',type,'"',":",'"',query,'"',"}",sep=""),"sparql")
+  res <- POST(neptune_base_url(ep), body=jsonQuery, content_type_json(),...)
+  content(res,as=match.arg(as))
 }
+
