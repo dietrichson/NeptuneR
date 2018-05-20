ce4883ffadba7a1532999e74d5a66d24128baf23
vlandaberry <victorialandaberry@gmail.com>
2018-05-20 19:30:32 -0300

    Add files via upload

---
+++ R/exampledata.R
@@ -0,0 +1,23 @@
+#' Vertex and Edges for the Tinkerpop graph example
+#'
+#'Two data frames, one for Vertex and Another with edges between those vertex from the example of Tinkerpop tutorial.
+#' @docType data
+#'
+#' @usage data(Vertex)
+#' @usage data(Edges)
+#'
+#' 
+#'
+#' @keywords datasets
+#'
+
+#'
+#' @source \href{http://tinkerpop.apache.org/docs/current/reference/}{ Archive}
+#'
+#' @examples
+#' data(Vertex)
+#' content <- head(vertex)
+#'
+#'
+"Vertex"
+"Edges"
