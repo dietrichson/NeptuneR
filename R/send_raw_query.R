d91e21462fbc7da76f21f8dbd11aaa62c09bf822
vlandaberry <victorialandaberry@gmail.com>
2018-05-19 23:12:22 -0300

    Add files via upload
    Updated ping and fixed gremlin query function.

--- R/send_raw_query.R
+++ R/send_raw_query.R
@@ -2,12 +2,11 @@
 #'
 #' @param ep Endpoint
 #' @param query The Query (in selected type) to be sent
-#' @param  type The type (language) of query to use
 #' @return the result from POST (in JSON)
 #' @export
 #' 
-neptune_send_json_query <- function(ep,query,type=c("gremlin","sparql"),as=c('parsed','text','raw'),...){
-  ifelse (type=="gremlin",jsonQuery<- paste0("{",'"',type,'"',":",'"',query,'"',"}",sep=""),"sparql")
+neptune_send_json_query <- function(ep,query,as=c('parsed','text','raw'),...){
+  jsonQuery<- paste0("{",'"',"gremlin",'"',":",'"',query,'"',"}",sep="")
   res <- POST(neptune_base_url(ep), body=jsonQuery, content_type_json(),...)
   content(res,as=match.arg(as))
 }
