e88ba18e035fcb8b898c64f4eb781b74a6622ffa
vlandaberry <victorialandaberry@gmail.com>
2018-05-21 20:55:32 -0300

    Add files via upload
    started function to load a data frame to neptune

--- R/neptune_load_data.R
+++ R/neptune_load_data.R
@@ -11,3 +11,35 @@
 neptune_load_data <- function(data, endpoint,S3_endpoint,AWS_accessKey,AWS_secret){
   cat('Not implemented Yet!')
 }
+
+
+#' Load data into your Neptune Instance from a data table
+#'
+#' @param data
+#' @param endpoint
+#' @param 
+#' @param 
+#' @param 
+#'
+#' @return parsed message from AWS
+#' @export
+neptune_load_data_frame <- function(data, endpoint ){
+ property <- grep("property",names(data))
+
+  addV<-matrix(0, nrow=nrow(data),ncol=6)
+  addV[,1]<-"g.addV('"
+  addV[,3]<-"')"
+  addV[,4]<-".property(id,'"
+  addV[,6] <- "')"
+  for(i in seq_len(nrow(data))){
+  addV[i,2] <-data$label[i]
+  addV[i,5]<-data$id[i]
+  }
+  
+addV <- apply(addV, 1, paste, collapse="")
+addV<-as.data.frame(addV)
+
+properties<-matrix(".property(", nrow=nrow(data), ncol=length(property))
+properties2 <- matrix( , nrow=nrow(data),ncol=lenght(property)
+
+}
